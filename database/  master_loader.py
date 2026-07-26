dc608c0b229ffae865874b28d234ca6e3cf817f2
Marquishampton23-gif <Marquishampton23@icloud.com>
2026-07-26 15:40:33 -0400

    Create   master_loader.py

---
+++ database/  master_loader.py
@@ -0,0 +1,35 @@
+Load Exit Velocity CSV
+↓
+
+Load Home Runs CSV
+↓
+
+Load Batted Ball CSV
+↓
+
+Load Bat Tracking CSV
+↓
+
+Load Swing Timing CSV
+↓
+
+Load Swing Path CSV
+↓
+
+Load Pitch Arsenal CSV
+↓
+
+Load Pitch Movement CSV
+↓
+
+Load Spin Direction CSV
+↓
+
+Load Positioning CSV
+↓
+
+Merge by MLBAM Player ID
+
+↓
+
+MASTER PLAYER PROFILE
