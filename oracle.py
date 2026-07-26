ba137c540956fef328df17bb2ad374ba45b138dc
Marquishampton23-gif <Marquishampton23@icloud.com>
2026-07-26 15:49:27 -0400

    Update oracle.py

--- oracle.py
+++ oracle.py
@@ -3,19 +3,24 @@ Oracle Engine
 Version 0.1 Alpha
 """
 
+from database.master_loader import MasterLoader
+
 from engine.dna_engine import DNAEngine
 from engine.matchup_engine import MatchupEngine
 from engine.opportunity_engine import OpportunityEngine
 from engine.confidence_engine import ConfidenceEngine
 from engine.statcast_engine import StatcastEngine
-from database.master_loader import MasterLoader
+
 
 class Oracle:
-    
-    self.loader = MasterLoader()
-self.database = self.loader.load_all()
 
     def __init__(self):
+
+        # Load Master Database
+        self.loader = MasterLoader()
+        self.database = self.loader.load_all()
+
+        # Load Oracle Engines
         self.statcast = StatcastEngine()
         self.dna = DNAEngine()
         self.matchup = MatchupEngine()
@@ -27,11 +32,13 @@ self.database = self.loader.load_all()
         return {
             "hitter": hitter,
             "pitcher": pitcher,
+            "database_loaded": self.database is not None,
             "status": "Oracle Connected"
         }
 
 
 if __name__ == "__main__":
+
     oracle = Oracle()
 
     print(
